b4f03ab0be2561e8a655ffe2c01d8d4c81f6f31a
Michael Daines <md@slowscan.net>
2026-09-25 09:32:12 -0400

    Add docc plugin

--- Package.swift
+++ Package.swift
@@ -20,6 +20,9 @@ let package = Package(
             targets: ["JapaneseNumberFormatStyle"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
